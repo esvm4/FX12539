48a01c7e56a6bd0fb77dd4ba64592cc40156f40f
evm <evicentem@protonmail.com>
2021-10-17 05:27:50 +0700

    Add Lab7.

---
+++ Lab/Lab7.js
@@ -0,0 +1,68 @@
+// Basic math
+var sum = 10 + 10;
+var difference = 45 - 33;
+var product0 = 8 * 10;
+var quotient = 66 / 33;
+
+// increment
+var myVar0 = 87;
+myVar0++;
+
+// decrement
+var myVar1 = 11;
+myVar   --;
+
+// Decimal
+var myDecimal = 5.7;
+
+// Multiply Two Decimals
+var product1 = 2.0 * 2.5;
+
+// Divide Decimals
+var quotient = 4.4 / 2.0;
+
+// Finding a Remainder
+var remainder=11%3;
+
+// Compound Assignment With Augmented Addition
+var a0 = 3;
+var b0 = 17;
+var c0 = 12;
+a0 += 12;
+b0 += 9;
+c0 += 7;
+
+// Compound Assignment With Augmented Subtraction
+var a1 = 11;
+var b1 = 9;
+var c1 = 3;
+a1 -= 6;
+b1 -= 15;
+c1 -= 1;
+
+// Compound Assignment With Augmented Multiplication
+var a2 = 5;
+var b2 = 12;
+var c2 = 4.6;
+a2 *= 5;
+b2 *= 3;
+c2 *= 10;
+
+// Compound Assignment With Augmented Division
+var a3 = 48;
+var b3 = 108;
+var c3 = 33;
+a3 /= 12;
+b3 /= 4;
+c3 /= 11;
+
+// Appending Variables to Strings
+var someAdjective="Fun";
+var myStr = "Learning to code is ";
+myStr+=someAdjective;
+
+// Find the Length of a String
+var lastNameLength = 0;
+var lastName = "Lovelace";
+lastNameLength = lastName.length;
+
